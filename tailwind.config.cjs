4fe5ff37085b7e4f9f090dc16b7cf1d294794eb8
adomfosugit <adomfosugit@gmail.com>
2023-10-30 19:38:40 +0000

    comit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('computer.jpg')",
+        "hero-pattern": "url('computer.png')",
         
       },
     },
